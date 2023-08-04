735374378dcbb0258cc42a1ab20298137ee42ebd
Chibundo Ilonze <chibundoilonze@gmail.com>
2023-08-04 14:46:56 +0100

    message

---
+++ littlelemon/littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by chibundo Ilonze on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
