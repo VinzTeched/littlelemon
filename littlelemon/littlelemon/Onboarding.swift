558edd29f2287e3e0a11bbf587ec9884d6859c0d
Chibundo Ilonze <chibundoilonze@gmail.com>
2023-08-09 13:26:56 +0100

    user profile

--- littlelemon/littlelemon/Onboarding.swift
+++ littlelemon/littlelemon/Onboarding.swift
@@ -10,6 +10,7 @@ import SwiftUI
 let kFirstName = "first name key"
 let kLastName = "last name key"
 let kEmail = "email key"
+let kIsLoggedIn = "kIsLoggedIn"
 
 struct Onboarding: View {
     @State var firstName = ""
@@ -28,17 +29,25 @@ struct Onboarding: View {
                 Divider()
                     .padding(.bottom, 20)
                 TextField("Email", text: $email)
+                    .autocorrectionDisabled()
                 Divider()
                     .padding(.bottom, 20)
                 Button("Register"){
                     if(firstName.isEmpty) {
-                        UserDefaults.standard.set(firstName, forKey: kFirstName)
+                        //UserDefaults.standard.set(firstName, forKey: kFirstName)
                     } else if (lastName.isEmpty) {
-                        UserDefaults.standard.set(lastName, forKey: kLastName)
+                        //UserDefaults.standard.set(lastName, forKey: kLastName)
                     } else if (email.isEmpty) {
-                        UserDefaults.standard.set(email, forKey: kEmail)
-                    } else {
+                        //UserDefaults.standard.set(email, forKey: kEmail)
+                    } else if (email.range(of:"^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$", options: .regularExpression) == nil) {
+                        //
+                    }
+                    else {
                         isLoggedIn = true
+                        UserDefaults.standard.set(firstName, forKey: kFirstName)
+                        UserDefaults.standard.set(lastName, forKey: kLastName)
+                        UserDefaults.standard.set(email, forKey: kEmail)
+                        UserDefaults.standard.set(true, forKey: kIsLoggedIn)
                     }
                 }
                 .padding(10)
@@ -48,6 +57,11 @@ struct Onboarding: View {
             }
             .navigationDestination(isPresented: $isLoggedIn) {
                 Home()
+            }
+            .onAppear() {
+                if(UserDefaults.standard.bool(forKey: kIsLoggedIn) == true) {
+                    isLoggedIn = true
+                }
             }
             .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
         }
--- littlelemon/littlelemon/UserProfile.swift
+++ littlelemon/littlelemon/UserProfile.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 struct UserProfile: View {
+    let firstName = UserDefaults.standard.string(forKey: "first name key")
+    let lastName = UserDefaults.standard.string(forKey: "last name key")
+    let email = UserDefaults.standard.string(forKey: "email key")
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         VStack {
-            Text("Personal Information")
-            Image("profile-image-placeholder")
+            Group {
+                Text("Personal Information")
+                    .bold()
+                Image("profile-image-placeholder")
+                    .clipShape(Capsule())
+                Divider()
+                    .padding(20)
+                Text(firstName ?? "null")
+                Divider()
+                    .frame(width: 0)
+                Text(lastName ?? "null")
+                Divider()
+                    .frame(width:0)
+                Text(email ?? "null")
+                Divider()
+                    .frame(width: 0.0, height: 20)
+                Button("Logout") {
+                    UserDefaults.standard.set(false, forKey: "kIsLoggedIn")
+                    dismiss()
+                }
+                .foregroundColor(.red)
+            }
+            Spacer()
+            
         }
     }
 }
